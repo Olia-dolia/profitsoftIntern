2e0ebc45bf7ffe0d1a869779b8a069a374fff9e8
Olia-dolia <dolechka4@gmail.com>
2022-11-20 00:31:06 +0200

    Variant 2 regex

--- src/main/java/FirstModule/SecondTask/ParseFromAndToXml/CustomXMLParser.java
+++ src/main/java/FirstModule/SecondTask/ParseFromAndToXml/CustomXMLParser.java
@@ -1,9 +1,10 @@
 package FirstModule.SecondTask.ParseFromAndToXml;
 
-import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomXMLParser {
 
@@ -11,9 +12,17 @@ public class CustomXMLParser {
         String inputFileName = "persons.xml";
         try {
             Scanner scanner = new Scanner(getFileFromResource(inputFileName));
-            while (scanner.hasNext()){
-                //function for parse
-                //function for write
+            while (scanner.hasNextLine()){
+                concatFullName(scanner.nextLine());
+                /*function concatName(scanner)
+                *   reg = [A-Z](for name)
+                *   reg = [A-Z](for surname)
+                *   if(scanner.findInLine("name"))
+                *       String name = scanner.findInLine(regN).concat(" " + scanner.findInLine(regS));
+                *       write(name);
+                *    else
+                *        write(scanner)
+                * */
             }
             scanner.close();
         } catch (Exception e){
@@ -22,15 +31,35 @@ public class CustomXMLParser {
 
     }
 
-    private static File getFileFromResource(String fileName) throws URISyntaxException {
+    private static BufferedInputStream getFileFromResource(String fileName) {
+        ClassLoader classLoader = CustomXMLParser.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
 
-        URL fileFromResource = CustomXMLParser.class.getClassLoader().getResource(fileName);
-        if (fileFromResource == null) {
+        if (inputStream == null) {
             throw new IllegalArgumentException("file not found! " + fileName);
         } else {
-            System.out.println("Its work");
-            return new File(fileFromResource.toURI());
+            System.out.println("Okay!");
+            return (BufferedInputStream) inputStream;
         }
+    }
+
+    private static String concatFullName(String input){
+        Pattern p = Pattern.compile("(name|surname)");
+        //Pattern p2 = Pattern.compile("(?:\")([^\"]+)(?:\")");
+        //StringBuilder stringBuilder = new StringBuilder(input);
+        Matcher match = p.matcher(input);
+        String surname = "";
+        String name = "";
+        if (match.find()){
+         name = Arrays.stream(input.split(" "))
+                  .filter(in -> in.matches("(?<=(name=))(?:\")([^\"]+)(?:\")|(?<=(name = ))(?:\")([^\"]+)(?:\")"))
+                  .toString();
+         surname = Arrays.stream(input.split(" "))
+                  .filter(in -> in.matches("(?<=(surname=))(?:\")([^\"]+)(?:\")|(?<=(surname = ))(?:\")([^\"]+)(?:\")"))
+                  .toString();
+            System.out.println(name+ "\n" + surname);
+        }
+        return input;
     }
 
 }
